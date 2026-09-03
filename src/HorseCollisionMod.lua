20c92156b2594b621a6e9fc97ec6996413062509
jrandall54 <jrandall1@ewu.edu>
2026-09-03 09:06:39 -0700

    chore: version 4.7.2

--- src/HorseCollisionMod.lua
+++ src/HorseCollisionMod.lua
@@ -66,10 +66,10 @@
 --
 -- @module HorseCollisionMod
 -- @author jrandall54
--- @release 4.7.1
+-- @release 4.7.2
 HorseCollisionMod = {}
 
-HorseCollisionMod.Version = "4.7.1"
+HorseCollisionMod.Version = "4.7.2"
 
 --- Loop generation counter, deliberately kept outside the table above.
 --
--- src/HorseCollisionMod/Rider.lua
+++ src/HorseCollisionMod/Rider.lua
@@ -16,7 +16,7 @@
 --
 -- @module HorseCollisionMod.Rider
 -- @author jrandall54
--- @release 4.7.1
+-- @release 4.7.2
 --- Whether this collision should count as a combat one.
 --
 -- Two independent signals, because neither alone is reliable:
